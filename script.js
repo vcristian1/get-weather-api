ed3cbc5ac4528044715f9bff208f12157a5bc070
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-06-24 16:08:01 -0500

    saved essential HTML elemtns to variables in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+const currentWeatherItemsEl = document.getElementById("current-weather-items");
+const timeZoneEl = document.getElementById("time-zone");
+const countryEl = document.getElementById("country");
+const weatherForecastEl = document.getElementById("weather-forecast")
+const currentTempEl = document.getElementById("current-temp");
+
+
