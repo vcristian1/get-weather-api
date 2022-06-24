2dadb3d3c8d29a6b4f2fa779ff6a2dd680f205d1
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-06-24 12:31:28 -0500

    added photoAPI.js to generate a random picture, and added basic styling to style.css

---
+++ photoAPI.js
@@ -0,0 +1,21 @@
+const numItemsToGenerate = 1;
+
+function renderItem() {
+  fetch(`https://source.unsplash.com/1600x900/?beach`).then((response) => {
+    let item = document.createElement("div");
+
+    item.classList.add("item");
+
+    item.innerHTML = `
+      <img class="beach-image" src="${response.url}" alt="beach image"/>
+    `;
+    
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?beach')";
+    let photo = document.querySelector("#photo");
+    photo.textContent = response.content;
+  });
+}
+for (let i = 0; i < numItemsToGenerate; i++) {
+  renderItem();
+}
