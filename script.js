fe01a15f47cdf2b5f9768aba1fb92b7317549ecb
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-06-24 17:53:04 -0500

    rewrote code for all times on the page so it is much cleaner to read

--- script.js
+++ script.js
@@ -8,55 +8,17 @@ const currentTempEl = document.getElementById("current-temp");
 const currentTime = document.getElementById("currentTime")
 
 setInterval(() => {
-    console.log(time)
-    displayCurrentTime();
-    displayCurrentDate();
-    displayCurrentDayForecast();
-    displaySecondDayForecast();
-    displayThirdDayForecast();
-    displayFourthDayForecast();
-    displayFifthDayForecast();
-    displaySixthDayForecast();
-    displaySeventhDayForecast();
-
+    displayAllTimes();
 }, 1000)
 
-function displayCurrentTime () {
+function displayAllTimes () {
     $("#currentTime").html(moment().format('LT'));
-}
-
-function displayCurrentDate () {
     $("#currentDate").html(moment().format('LL'));
-}
-
-function displayCurrentDayForecast () {
     $("#day").html(moment().format('dddd'));
-}
-
-function displaySecondDayForecast () {
     $("#secondday").html(moment().add(1, 'days').calendar());
-}
-function displayThirdDayForecast () {
     $("#thirdday").html(moment().add(2, 'days').calendar());
-}
-function displayFourthDayForecast () {
     $("#fourthday").html(moment().add(3, 'days').calendar());
-}
-function displayFifthDayForecast () {
     $("#fifthday").html(moment().add(4, 'days').calendar());
-}
-function displaySixthDayForecast () {
     $("#sixthday").html(moment().add(5, 'days').calendar());
-}
-function displaySeventhDayForecast () {
     $("#seventhday").html(moment().add(6, 'days').calendar());
-}
-
-
-
-
-
-
-function displayTomorrowsDate () {
-
 }
